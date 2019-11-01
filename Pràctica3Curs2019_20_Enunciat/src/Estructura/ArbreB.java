6935d07aeaf3e8b1f3c08c0cc3f1b73369d2d5e1
ElRiseaciones <57240754+ElRiseaciones@users.noreply.github.com>
2019-11-01 01:53:24 +0100

    Update ArbreB.java

--- Pràctica3Curs2019_20_Enunciat/src/Estructura/ArbreB.java
+++ Pràctica3Curs2019_20_Enunciat/src/Estructura/ArbreB.java
@@ -13,19 +13,19 @@ public class ArbreB {
 		String contents;
 		ArbreB yes, no;	
 		NodeA(String contents) {
-            // Constructor 1. Inicialitza als atributys yes i no a null
-            this.contents = contents;
-            yes = null;
-            no = null;
-        }
+            	// Constructor 1. Inicialitza als atributys yes i no a null
+            	this.contents = contents;
+            	yes = null;
+            	no = null;
+        	}
 
-        // PROVISIONAL
-        NodeA(String pregunta, ArbreB a1, ArbreB a2) {
-            // Constructor 2. Crea el node i l'inicialitza amb els paràmetres
-            this.contents = pregunta;
-            yes = a1;
-            no = a2;
-	}
+        	// PROVISIONAL
+        	NodeA(String pregunta, ArbreB a1, ArbreB a2) {
+            	// Constructor 2. Crea el node i l'inicialitza amb els paràmetres
+            	this.contents = pregunta;
+            	yes = a1;
+            	no = a2;
+		}
 	}
 	// Atributs: Taula de 2 posicions
 	private NodeA[] root;

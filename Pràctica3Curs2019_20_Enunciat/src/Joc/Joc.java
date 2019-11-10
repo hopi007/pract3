0a707119715d13a288ee83236e18db389c344df0
hopi007 <57240560+hopi007@users.noreply.github.com>
2019-11-10 22:29:09 +0100

    Update Joc.java

--- Pràctica3Curs2019_20_Enunciat/src/Joc/Joc.java
+++ Pràctica3Curs2019_20_Enunciat/src/Joc/Joc.java
@@ -1,74 +1,177 @@
 package Joc;
 
+import Estructura.ArbreB;
+import Keyboard.Keyboard;
+
 public class Joc {
-	
-	private boolean volsJugar;
-	private String resposta;
-	private Keyboard consola = new Keyboard();
-	
+
+	private static boolean volsJugar;
+	private static String resposta;
+	static Keyboard consola = new Keyboard();
+	private static ArbreB arbre;
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub	
+		// TODO Auto-generated method stub
+
 		System.out.println("Welcome to wonderful GUESS THE ANIMAL");
 		System.out.println("-------------------------------------");
 		System.out.println();
 		System.out.print("Vols carregar un fitxer?");
 		resposta = consola.readString();
 		System.out.println();
-		while(!resposta.equals("SI")&&!resposta.equals("Si")&&!resposta.equals("sI")&&!resposta.equals("si")&&!resposta.equals("NO")&&!resposta.equals("No")&&!resposta.equals("nO")&&!resposta.equals("no")){
+		while (!resposta.equalsIgnoreCase("si") || !resposta.equalsIgnoreCase("no")) {
 			System.out.print("Resposta no valida. Vols carregar un fitxer?");
-			resposta= consola.readString();
+			resposta = consola.readString();
 			System.out.println();
 		}
-		volsJugar=true;
-		while(volsJugar){
-			if(resposta.equals("SI")||resposta.equals("sI")||resposta.equals("Si")||resposta.equals("si"))
+		volsJugar = true;
+		while (volsJugar) {
+			if (resposta.equalsIgnoreCase("si"))
 				jocFitxer();
-			else jocSenseFitxer();
+			else
+				jocSenseFitxer();
 		}
-		
+
 		System.out.print("ADEU!");
 	}
-	
-	
-	private static void jocFitxer(){
-	
+
+	private static void jocFitxer() {
+
 	}
-	
-	private static void jocSenseFitxer(){
-		
+
+	private static void jocSenseFitxer() {
+
 		String pregunta, yes, no, resposta;
-		
+
 		System.out.println("Per començar cal introduir una primera pregunta amb dues respostes");
 		System.out.println("------------------------------------------------------------------");
-		System.out.printl();
+		System.out.println();
 		System.out.print("  Indica la pregunta de l'arrel de l'arbre: ");
-		pregunta= consola.readString();
-		
-		//falta el metodo para añadir el ? al final por si acaso no lo pone el usuario
-		
+		pregunta = consola.readString();
+		pregunta = checkQues(pregunta);
+
 		System.out.println();
 		System.out.print("  Indica el nom de l'animal de la resposta afirmativa: ");
-		yes= consola.readString();
+		yes = consola.readString();
+		yes = checkAns(yes);
+
 		System.out.println();
 		System.out.print("  Indica el nom de l'animal de la resposta negativa: ");
-		no= consola.readString();
-		
+		no = consola.readString();
+		no = checkAns(no);
+
 		System.out.println();
 		System.out.println();
-		
+
 		System.out.println("JUEGUEM!!!!");
 		System.out.println();
-		
-		System.out.print(//la pregunta del node);
-		resposta= consola.readString();
-			while(!resposta.equals("SI")&&!resposta.equals("Si")&&!resposta.equals("sI")&&!resposta.equals("si")&&!resposta.equals("NO")&&!resposta.equals("No")&&!resposta.equals("nO")&&!resposta.equals("no")){
-				System.out.print("Resposta no valida"//+ la pregunta del node);
-				resposta= consola.readString();
+
+		arbre = crearArbre(pregunta, yes, no); //No se como crear los arboles, si los creas, en principio debería funcionar todo. Mira metodo.
+
+		while (volsJugar) {
+
+			System.out.print("\t" + arbre.getContents());
+			resposta = consola.readString();
+			System.out.println();
+
+			while (!resposta.equalsIgnoreCase("si") || !resposta.equalsIgnoreCase("no")) {
+				System.out.print("\tResposta no valida. " + arbre.getContents());
+				resposta = consola.readString();
+				System.out.println();
 			}
-		
-		if(resposta.equals("SI")||resposta.equals("sI")||resposta.equals("Si")||resposta.equals("si")){
+
+			estarJugant(arbre.getContents(), resposta);
 		}
-			
+	}
+
+	private static void estarJugant(String pregunta, String resposta) {
+
+		if (resposta.equalsIgnoreCase("si")) {
+			if (arbre.atAnswer()) {
+				respostaFinalCorrecta();
+				continuarJugar();
+			}
+			arbre.moveToYes();
+		} else {
+			if (arbre.atAnswer()) {
+				respostaFinalIncorrecta();
+				continuarJugar();
+			}
+			arbre.moveToNo();
+		}
+	}
+
+	private static void respostaFinalCorrecta() {
+		System.out.println("Gooooood. ENCERTAT!!!");
+	}
+
+	private static void respostaFinalIncorrecta() {
+		System.out.println("Uuuups HE FALLAT!!!");
+		System.out.print("\tplis, diguem el nom de l'animal que has pensat: ");
+		String right = consola.readString();
+		right = checkAns(right);
+		System.out.println();
+		System.out.print("\tplis, diguem una pregunta que correspon a aquest animal: ");
+		String pregunta = consola.readString();
+		pregunta = checkQues(pregunta);
+		System.out.println();
+
+		arbre.improve(pregunta, right);
+	}
+
+	private static void continuarJugar() {
+		System.out.print("\tVols jugar altre cop? ");
+		resposta = consola.readString();
+		System.out.println();
+		while (!resposta.equalsIgnoreCase("si") || !resposta.equalsIgnoreCase("no")) {
+			System.out.print("\tResposta no valida. Vols jgar altre cop? ");
+			resposta = consola.readString();
+			System.out.println();
+		}
+
+		if (resposta.equalsIgnoreCase("si")) {
+			volsJugar = true;
+		} else {
+			volsJugar = false;
+
+			System.out.print("Vols emmagatzemar 'el coneixement' en un fitxer? ");
+			String guardar = consola.readString();
+			if (guardar.equalsIgnoreCase("si"))
+				guardarFitxer(arbre);
+		}
+	}
+
+	private static String checkQues(String preg) {
+		if (preg.charAt(preg.length() - 1) != '?')
+			preg = preg + "?";
+		return preg.toUpperCase();
+	}
+
+	private static String checkAns(String ans) {
+		if (ans.charAt(ans.length() - 1) == '?')
+			ans = ans.substring(0, ans.length() - 1);
+		return ans.toUpperCase();
+	}
+
+	private static ArbreB crearArbre(String pregunta, String yes, String no) { 
+		// falta implementarlo, sería el que crea el arbol donde jugamos
+		return null;
+	}
+
+	private static void guardarFitxer(ArbreB arbre) {
+		System.out.println();
+		System.out.print("Nom del fitxer? ");
+		String nomFitxer = consola.readString();
+
+		if (nomFitxer.substring(nomFitxer.length() - 4, nomFitxer.length()).equalsIgnoreCase(".txt"))
+			nomFitxer = nomFitxer.toUpperCase() + ".TXT";
+
+		try {
+			arbre.save(nomFitxer);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
